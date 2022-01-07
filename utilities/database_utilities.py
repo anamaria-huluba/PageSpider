e31b0e091a9c8e7cbfc00e1e0837960385fb19e2
anamaria-huluba <anamaria.huluba@gmail.com>
2022-01-07 12:33:53 +0000

    fixed formatting issues

--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -2,8 +2,7 @@ def create_database(database_path: str):
     # TODO: generate database
     pass
 
+
 def save_words_to_database(database_path: str, words_list: list):
     # TODO: save words to the database
-   pass
-
-
+    pass
